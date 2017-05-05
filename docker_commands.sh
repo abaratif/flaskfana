703d67102a4d73665d347db5ffe121e75ba33577
Arman Baratifar <ab405h@att.com>
2017-05-05 12:17:08 -0500

    Added docker command for Grafana

--- docker_commands.sh
+++ docker_commands.sh
@@ -6,3 +6,21 @@
 # -e PRE_CREATE_DB=telegraf \
 # --name influxdb \
 #  influxdb:0.13
+
+# Run the app image that includes telegraf
+docker build -t flaskfana_image . 
+docker run -p 80:80 -p 8125:8125 -t -d --link=influxdb:influxsrv --name flaskfana_container  flaskfana_image
+
+## Run the Grafana image
+
+docker run -d -p 3000:3000 \
+-e HTTP_USER=admin \
+-e HTTP_PASS=admin \
+-e INFLUXDB_HOST=influxsrv \
+-e INFLUXDB_PORT=8086 \
+-e INFLUXDB_NAME=telegraf \
+-e INFLUXDB_USER=root \
+-e INFLUXDB_PASS=root \
+--link=influxdb:influxsrv  \
+--name grafana \
+grafana/grafana:4.2.0
